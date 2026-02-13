1cd6fd0543840d3687cd8fdd6aa1560d8c024183
Baron <realbarondedios@gmail.com>
2026-02-13 16:20:25 +0800

    Done with Flex and FlexDirection

---
+++ react-native-flexbox/App.js
@@ -0,0 +1,11 @@
+// import Flex from "./components/Flex";
+
+// export default function App() {
+//   return <Flex />;
+// }
+
+import FlexDirection from "./components/FlexDirection";
+
+export default function App() {
+  return <FlexDirection />;
+}
