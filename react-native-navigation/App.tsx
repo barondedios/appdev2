a037108ac624c2b5ec5259dbb642f9ee0c18643b
barondedios <realbarondedios@gmail.com>
2026-04-24 17:39:37 +0800

    Imported and configured TypeScript in App file

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -4,29 +4,23 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import DetailsScreen from "./screens/DetailsScreen";
 
-import { RootStackParamList } from "./types";
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
 
+        {/* HOME SCREEN */}
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{
-            title: "Home Screen",
-          }}
         />
 
+        {/* DETAILS SCREEN */}
         <Stack.Screen
           name="Details"
           component={DetailsScreen}
-          options={{
-            title: "Details Screen",
-          }}
         />
 
       </Stack.Navigator>
