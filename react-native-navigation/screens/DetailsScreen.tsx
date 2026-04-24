15f48d9d6cb165489c5fa7b49d144e23beafd38b
barondedios <realbarondedios@gmail.com>
2026-04-24 17:36:07 +0800

    Done with updating params

--- react-native-navigation/screens/DetailsScreen.tsx
+++ react-native-navigation/screens/DetailsScreen.tsx
@@ -1,7 +1,19 @@
-export default function DetailsScreen({ route }: any) {
+import { View, Text, Button } from "react-native";
+
+export default function DetailsScreen({ route, navigation }: any) {
   const { name } = route.params;
 
   return (
-    <Text>Hello {name}</Text>
+    <View>
+      <Text>Hello {name}</Text>
+
+      {/* UPDATE PARAMS */}
+      <Button
+        title="Update Name"
+        onPress={() =>
+          navigation.setParams({ name: "Updated Baron" })
+        }
+      />
+    </View>
   );
 }
