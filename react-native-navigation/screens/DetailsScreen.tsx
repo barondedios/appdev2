546127046365c3f07466bf53d510d51a09d4cd19
barondedios <realbarondedios@gmail.com>
2026-04-24 17:36:41 +0800

    Done with passing params to nested screen

--- react-native-navigation/screens/DetailsScreen.tsx
+++ react-native-navigation/screens/DetailsScreen.tsx
@@ -1,24 +1,12 @@
-import { View, Text, Button } from "react-native";
+import { View, Text } from "react-native";
 
-export default function DetailsScreen({ navigation, route }: any) {
+export default function DetailsScreen({ route }: any) {
   const { name } = route.params;
 
   return (
     <View>
       <Text>Details Screen</Text>
       <Text>Hello {name}</Text>
-
-      {/* PASS PARAMS BACK TO HOME SCREEN */}
-      <Button
-        title="Send Message Back to Home"
-        onPress={() =>
-          navigation.navigate({
-            name: "Home",
-            params: { message: "Hello from Details Screen!" },
-            merge: true,
-          })
-        }
-      />
     </View>
   );
 }
--- react-native-navigation/screens/HomeScreen.tsx
+++ react-native-navigation/screens/HomeScreen.tsx
@@ -1,17 +1,29 @@
 import { View, Text, Button } from "react-native";
 
-export default function HomeScreen({ navigation, route }: any) {
-  const message = route.params?.message;
-
+export default function HomeScreen({ navigation }: any) {
   return (
     <View>
       <Text>Home Screen</Text>
 
-      {message && <Text>{message}</Text>}
-
+      {/* NORMAL NAVIGATION */}
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate("Details", { name: "Baron" })}
+        onPress={() =>
+          navigation.navigate("Details", { name: "Baron" })
+        }
+      />
+
+      {/* NESTED PARAMS NAVIGATION */}
+      <Button
+        title="Nested Navigation to Details"
+        onPress={() =>
+          navigation.navigate("Home", {
+            screen: "Details",
+            params: {
+              name: "Nested Baron",
+            },
+          })
+        }
       />
     </View>
   );
